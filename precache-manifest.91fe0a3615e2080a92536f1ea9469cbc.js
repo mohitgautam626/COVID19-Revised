e8115d1b1a6a12b6786af32c3ad114624635b4be
mohitgautam626 <mohitgautam626@gmail.com>
2020-06-03 12:04:16 +0530

    Updates

---
+++ precache-manifest.91fe0a3615e2080a92536f1ea9469cbc.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f83e18dbff7ab1ce9a56ce2841c922e",
+    "url": "/COVID19-Revised/index.html"
+  },
+  {
+    "revision": "4827a319e10eca77dcb1",
+    "url": "/COVID19-Revised/static/css/main.7de89084.chunk.css"
+  },
+  {
+    "revision": "694aef395db7ac997af0",
+    "url": "/COVID19-Revised/static/js/2.6cbf2e92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/COVID19-Revised/static/js/2.6cbf2e92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4827a319e10eca77dcb1",
+    "url": "/COVID19-Revised/static/js/main.99bc8f8d.chunk.js"
+  },
+  {
+    "revision": "2e987a13eef3ba980299",
+    "url": "/COVID19-Revised/static/js/runtime-main.bdd749f3.js"
+  },
+  {
+    "revision": "c03a85714b17674809c125a6484441d1",
+    "url": "/COVID19-Revised/static/media/Andhra Pradesh.c03a8571.png"
+  },
+  {
+    "revision": "206b45b522d348151adfa20dbae90085",
+    "url": "/COVID19-Revised/static/media/Arunachal Pradesh.206b45b5.png"
+  },
+  {
+    "revision": "f6e931350169d7023ef11203a43b175c",
+    "url": "/COVID19-Revised/static/media/Assam.f6e93135.png"
+  },
+  {
+    "revision": "882016000442064caab4182a797e5a97",
+    "url": "/COVID19-Revised/static/media/Bihar.88201600.png"
+  },
+  {
+    "revision": "656bcddc134750165a152cf86be9e6d4",
+    "url": "/COVID19-Revised/static/media/Chhattisgarh.656bcddc.png"
+  },
+  {
+    "revision": "2b8e2a93feb41f48eda46820ddae0a01",
+    "url": "/COVID19-Revised/static/media/Delhi.2b8e2a93.jfif"
+  },
+  {
+    "revision": "31ba77874f2c12ec1ead5fcb654de131",
+    "url": "/COVID19-Revised/static/media/Goa.31ba7787.png"
+  },
+  {
+    "revision": "77d0562995f1e871f5730ea47186db98",
+    "url": "/COVID19-Revised/static/media/Gujarat.77d05629.png"
+  },
+  {
+    "revision": "2f6c430ef7d3f3c19884639201a35aef",
+    "url": "/COVID19-Revised/static/media/Himachal Pradesh.2f6c430e.gif"
+  },
+  {
+    "revision": "2228aa2f6a2d0124022ccd64730cf173",
+    "url": "/COVID19-Revised/static/media/Jammu and Kashmir.2228aa2f.png"
+  },
+  {
+    "revision": "9987371fb874514cfcd19a5498ea6464",
+    "url": "/COVID19-Revised/static/media/Jharkhand.9987371f.jpg"
+  },
+  {
+    "revision": "9713e67c59aa5a59eb58af10f0deafa2",
+    "url": "/COVID19-Revised/static/media/Karnataka.9713e67c.png"
+  },
+  {
+    "revision": "edcb2c5be284f4ab7c85d6dc5f1d0e75",
+    "url": "/COVID19-Revised/static/media/Kerala.edcb2c5b.png"
+  },
+  {
+    "revision": "b977ed2c8692cc46977924b5239805ad",
+    "url": "/COVID19-Revised/static/media/Madhya Pradesh.b977ed2c.png"
+  },
+  {
+    "revision": "784b668a59981d2305e704d7344fb46a",
+    "url": "/COVID19-Revised/static/media/Maharashtra.784b668a.png"
+  },
+  {
+    "revision": "daf8f2e03a18bcffc863abd16fed6e61",
+    "url": "/COVID19-Revised/static/media/Manipur.daf8f2e0.jfif"
+  },
+  {
+    "revision": "6789ff825365a42d3f3eb3b090c490a4",
+    "url": "/COVID19-Revised/static/media/Meghalaya.6789ff82.png"
+  },
+  {
+    "revision": "a369c809cb1bd32b43af615b23c96102",
+    "url": "/COVID19-Revised/static/media/Nagaland.a369c809.png"
+  },
+  {
+    "revision": "1fceed0e8c99f189e460362fa00b3cd2",
+    "url": "/COVID19-Revised/static/media/Odisha.1fceed0e.png"
+  },
+  {
+    "revision": "e27ef3696428a74b8c87c4e038fb1420",
+    "url": "/COVID19-Revised/static/media/Punjab.e27ef369.png"
+  },
+  {
+    "revision": "df5d8219a3448c5fa21cd35ac1454b61",
+    "url": "/COVID19-Revised/static/media/Sikkim.df5d8219.jpg"
+  },
+  {
+    "revision": "d89483967ae4402f0c7dec9b8a63570d",
+    "url": "/COVID19-Revised/static/media/Tamil Nadu.d8948396.jfif"
+  },
+  {
+    "revision": "911c1cc3702e26938f69b68ae4696cb9",
+    "url": "/COVID19-Revised/static/media/Telangana.911c1cc3.webp"
+  },
+  {
+    "revision": "7b98af64cecfe206c84943826d55f8c0",
+    "url": "/COVID19-Revised/static/media/Tripura.7b98af64.jfif"
+  },
+  {
+    "revision": "0b6ce1085ff39ded1c132bfb045a28df",
+    "url": "/COVID19-Revised/static/media/Uttar Pradesh.0b6ce108.png"
+  },
+  {
+    "revision": "e145094e5d9e8b3f3de7b8735a713714",
+    "url": "/COVID19-Revised/static/media/West Bengal.e145094e.png"
+  },
+  {
+    "revision": "3ad22cc99490d0e75a4795d5ef8ba5e3",
+    "url": "/COVID19-Revised/static/media/emblem.3ad22cc9.png"
+  }
+]);
